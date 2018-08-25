997a7afbcddf2daae926aae14728afc223aa8407
florije4ex <florije4ex@gmail.com>
2018-08-25 12:01:58 +0800

    Update: 调整测试代码，带验证交易是否成功。

--- ripple/main.js
+++ ripple/main.js
@@ -1,4 +1,8 @@
 'use strict';
+//
+// https://developers.ripple.com/list-xrp-as-an-exchange.html
+// https://blog.csdn.net/vohyeah/article/details/80868313
+// https://developers.ripple.com/get-started-with-rippleapi-for-javascript.html
 
 const RippleAPI = require('ripple-lib').RippleAPI;
 const request = require('request-promise');
@@ -6,6 +10,18 @@ const sleep = require('sleep');
 
 const api = new RippleAPI({
     server: 'wss://s.altnet.rippletest.net:51233'
+});
+
+api.on('error', (errorCode, errorMessage) => {
+    console.log(errorCode + ': ' + errorMessage);
+});
+
+api.on('connected', () => {
+    console.log('api connected');
+});
+
+api.on('disconnected', () => {
+    console.log('disconnected');
 });
 
 function genAddr() {
@@ -22,13 +38,18 @@ async function getAcc() {
 }
 
 async function getAccInfo(addr) {
-    await api.connect();
     try {
         return await api.getAccountInfo(addr);
     } catch (e) {
         console.log(e);
-    } finally {
-        await api.disconnect();
+    }
+}
+
+async function getSetting(addr) {
+    try {
+        return await api.getSettings(addr);
+    } catch (e) {
+        console.log(e);
     }
 }
 
@@ -36,24 +57,63 @@ async function pay(from, to, amount, secret) {
     const payment = {
         "source": {
             "address": from,
-            "amount": {
+            "maxAmount": {
                 "value": amount,
                 "currency": "XRP"
             }
         },
         "destination": {
+            "tag": 10086,
             "address": to,
-            "minAmount": {
+            "amount": {
                 "value": amount,
                 "currency": "XRP"
             }
         }
     };
-    await api.connect();
     try {
+        let ledger = await api.getLedger();
         let prepared = await api.preparePayment(from, payment);
         console.log('prepared:');
         console.log(prepared);
+        await sendTxn(prepared, secret, ledger);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function setting(addr, secret) {
+    try {
+        let ledger = await api.getLedger();
+        const settings = {requireDestinationTag: true};
+        const instructions = {maxLedgerVersionOffset: 100};
+        let prepared = await api.prepareSettings(addr, settings, instructions);
+        console.log('prepared:');
+        console.log(prepared);
+        await sendTxn(prepared, secret, ledger);
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function verify(hash, options) {
+    try {
+        let res = await api.getTransaction(hash, options);
+        console.log(res);
+        return res.outcome.result === 'tesSUCCESS';
+    } catch (e) {
+        if (e instanceof api.errors.PendingLedgerVersionError) {
+            return false;
+        }
+    }
+}
+
+async function sendTxn(prepared, secret, ledger) {
+    const options = {
+        minLedgerVersion: ledger.ledgerVersion,
+        maxLedgerVersion: prepared.instructions.maxLedgerVersion
+    };
+    try {
         let signedTxn = await api.sign(prepared.txJSON, secret);
         console.log('signedTxn:');
         console.log(signedTxn);
@@ -63,24 +123,36 @@ async function pay(from, to, amount, secret) {
         if (result.resultCode === 'tesSUCCESS') {
             console.log('txHash:', signedTxn.id);
         }
+        while (!await verify(signedTxn.id, options)) {
+            sleep.sleep(3);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+(async () => {
+    try {
+        await api.connect();
+        console.log(genAddr());
+        let acc1 = await getAcc();
+        console.log(acc1);
+        let acc2 = await getAcc();
+        console.log(acc2);
+        sleep.sleep(10);
+        console.log(await getAccInfo(acc1.account.address));
+        console.log(await getAccInfo(acc2.account.address));
+        console.log(await getSetting(acc2.account.address));
+        await setting(acc2.account.address, acc2.account.secret);
+        console.log(await getSetting(acc2.account.address));
+        await pay(acc1.account.address, acc2.account.address, "10", acc1.account.secret);
+        console.log(await getAccInfo(acc1.account.address));
+        console.log(await getAccInfo(acc2.account.address));
     } catch (e) {
         console.log(e);
     } finally {
         await api.disconnect();
     }
-}
-
-(async() => {
-    // let addr = api.generateAddress();
-    let acc1 = await getAcc();
-    console.log(acc1);
-    let acc2 = await getAcc();
-    console.log(acc2);
-    sleep.sleep(10);
-    console.log(await getAccInfo(acc1.account.address));
-    console.log(await getAccInfo(acc2.account.address));
-    await pay(acc1.account.address, acc2.account.address, "10", acc1.account.secret);
-    sleep.sleep(10);
-    console.log(await getAccInfo(acc1.account.address));
-    console.log(await getAccInfo(acc2.account.address));
-})().catch((e) => {console.log(e);});
+})().catch((e) => {
+    console.log(e);
+});
