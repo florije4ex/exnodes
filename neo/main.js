17c1ed8be50b88fce8e08820315f796f35746152
florije4ex <florije4ex@gmail.com>
2018-10-17 15:11:01 +0800

    Update: 添加gas部分的转账。

--- neo/main.js
+++ neo/main.js
@@ -2,17 +2,16 @@
 
 const Neon = require('@cityofzion/neon-js');
 
-async function send(toAddress, amount) {
+async function sendAssert(assertId, receiver, amount) {
     const privateKey = 'KxDgvEKzgSBPPfuVfw67oPQBSjidEiqTHURKSDL1R7yGaGYAeYnr';
-    const neoAssertId = 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b';
     const rpcUrl = 'http://localhost:30333';
     const scanUrl = 'http://localhost:4000/api/main_net';
 
     let account = new Neon.wallet.Account(privateKey);
     let intent = {
-        assetId: neoAssertId,
+        assetId: assertId,
         value: new Neon.u.Fixed8(amount),
-        scriptHash: Neon.wallet.getScriptHashFromAddress(toAddress)};
+        scriptHash: Neon.wallet.getScriptHashFromAddress(receiver)};
     let intents = [intent];
     let balance = await Neon.api.neoscan.getBalance(scanUrl, account.address);
     let tmpTx = new Neon.tx.ContractTransaction({ outputs: intents });
@@ -39,6 +38,10 @@ async function send(toAddress, amount) {
 }
 
 (async () => {
-    const address = 'Ab6frjKP5EJw7WPxfU4aR5wqTMtfMLrQz6';
-    await send(address, 1);
+    const assertIds = {
+        NEO: 'c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b',
+        GAS: '602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7'
+    };
+    const receiver = 'Ab6frjKP5EJw7WPxfU4aR5wqTMtfMLrQz6';
+    await sendAssert(assertIds.GAS, receiver, 1);
 })();
